ffe55a17647d2d0a7c9027e0ca84985059532b37
Haysten Dcosta <haystendc@gmail.com>
2023-02-11 02:18:12 +0530

    Fixed the Student Search Bugs

--- Projects in C++/Project01/Project01.cpp
+++ Projects in C++/Project01/Project01.cpp
@@ -5,9 +5,11 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <sstream>
+
 using namespace std;
 
-int n;
+int n, choice;
 
 class CollegeDetails { //Main Abstract class...
     protected :
@@ -23,15 +25,13 @@ class CollegeDetails { //Main Abstract class...
         virtual void searchDetails() = 0;
         virtual void setDetails() = 0;  //This function can be defined in own way for students and teachers section...
 };
-
-
 class CollegeStudents : public CollegeDetails {  //STUDENTS CLASS **********************************
     void setDetails() {
         cin.ignore();
-        cout << "\n\t\t\tEnter Name : "; cin >> name;
-        cout << "\t\t\tEnter Roll Number : "; cin >> roll;
-        cout << "\t\t\tEnter College Email : "; cin >> email;
-        cout << "\t\t\tEnter Branch : "; cin >> branch;
+        cout << "\n\t\t\tEnter Name : "; getline(cin, name);
+        cout << "\t\t\tEnter Roll Number : "; getline(cin, roll);
+        cout << "\t\t\tEnter Email : "; getline(cin, email);
+        cout << "\t\t\tEnter Branch : "; getline(cin, branch);
     }
     void createFile() {  //FOR STUDENTS
         ofstream out;
@@ -44,36 +44,135 @@ class CollegeStudents : public CollegeDetails {  //STUDENTS CLASS **************
     }
     void searchDetails() {  //FOR STUDENTS
         ifstream in;
-        string searchsName;
-        cout << "\n\t\t\tEnter Name of student to be searched : "; cin.ignore();  //FLUSHING STREAM...
-        getline(cin, searchsName);
-
+        string searchName, searchRollno, searchBranch, searchEmail;
+        string str, str1, str2, str3, str4;
         in.open("Students.txt");
         int count = {};
-        string str1, str2, str3, str4;
-        while(in >> str1 >> str2 >> str3 >> str4) {
-            if(searchsName == str1) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
-                count++;
-                cout << "\n\n\t\t\tRequired Details : \n";
-                cout << "\n"
-                    << "\t\t\tName          : " << setw(25) << str1 << endl
-                    << "\t\t\tRoll no       : " << setw(25) << str2 << endl
-                    << "\t\t\tBranch        : " << setw(25) << str3 << endl
-                    << "\t\t\tCollege Email : " << setw(25) << str4 << endl;
+        cout << "\n\t\t\t1 <- Search student by 'Name'....." << endl
+             << "\t\t\t2 <- Search student by 'Roll number'....." << endl
+             << "\t\t\t3 <- Search student by 'Email'....." << endl
+             << "\t\t\t4 <- Search student by 'Branch'....." << endl
+             << "\t\t\t0 <- To return to previous page....." << endl
+             << "\n\t\t ..... Enter your choice : "; cin >> choice;
+        if(choice == 1) {
+            cout << "\n\t\t\tEnter Name of student to be searched : "; cin.ignore();  //FLUSHING STREAM...
+            getline(cin, searchName);
+            while(!in.eof()) {          /* MAIN CORRECT CODE, commented to try something diff...below!! */
+                getline(in, str1);
+                getline(in, str2);
+                getline(in, str3);
+                getline(in, str4);
+                if(searchName == str1) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
+                    count++;
+                    if(count < 2) { cout << "\n\n\t\t\tRequired Details : \n"; }
+                    cout << "\n"
+                         << "\t\t\tName          : " << setw(25) << str1 << endl
+                         << "\t\t\tRoll no       : " << setw(25) << str2 << endl
+                         << "\t\t\tCollege Email : " << setw(25) << str3 << endl
+                         << "\t\t\tBranch        : " << setw(25) << str4 << endl;
+                }
             }
+            // while(getline(in, str)) {  /* This doesnt work, but try and check..... creates a problem when using sstream*/
+            //     istringstream ss(str);
+            //     ss >> str1 >> str2 >> str3 >> str4;
+            //     if(searchName == str1) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
+            //         count++;
+            //         cout << "\n\n\t\t\tRequired Details : \n";
+            //         cout << "\n"
+            //              << "\t\t\tName          : " << setw(25) << str1 << endl
+            //              << "\t\t\tRoll no       : " << setw(25) << str2 << endl
+            //              << "\t\t\tCollege Email : " << setw(25) << str3 << endl
+            //              << "\t\t\tBranch        : " << setw(25) << str4 << endl;
+            //     }
+            // }
+
+
+
+
+            in.close();
+            if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
         }
-        in.close();
-        if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
+        else if(choice == 2) {
+            cout << "\n\t\t\tEnter Roll Number of student to be searched : "; cin.ignore();  //FLUSHING STREAM...
+            getline(cin, searchRollno);
+            while(!in.eof()) {
+                getline(in, str1);
+                getline(in, str2);
+                getline(in, str3);
+                getline(in, str4);
+                if(searchRollno == str2) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
+                    count++;
+                    if(count < 2) { cout << "\n\n\t\t\tRequired Details : \n"; }
+                    cout << "\n"
+                            << "\t\t\tName          : " << setw(25) << str1 << endl
+                            << "\t\t\tRoll no       : " << setw(25) << str2 << endl
+                            << "\t\t\tCollege Email : " << setw(25) << str3 << endl
+                            << "\t\t\tBranch        : " << setw(25) << str4 << endl;
+                }
+            }
+            in.close();
+            if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
+        }
+        else if(choice == 3) {
+            cout << "\n\t\t\tEnter Email of student to be searched : "; cin.ignore();  //FLUSHING STREAM...
+            getline(cin, searchEmail);
+            while(!in.eof()) {
+                getline(in, str1);
+                getline(in, str2);
+                getline(in, str3);
+                getline(in, str4);
+                if(searchEmail == str3) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
+                    count++;
+                    if(count < 2) { cout << "\n\n\t\t\tRequired Details : \n"; }
+                    cout << "\n"
+                            << "\t\t\tName          : " << setw(25) << str1 << endl
+                            << "\t\t\tRoll no       : " << setw(25) << str2 << endl
+                            << "\t\t\tCollege Email : " << setw(25) << str3 << endl
+                            << "\t\t\tBranch        : " << setw(25) << str4 << endl;
+                }
+            }
+            in.close();
+            if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
+        }
+        else if(choice == 4) {
+            cout << "\n\t\t\tEnter Branch of student to be searched : "; cin.ignore();  //FLUSHING STREAM...
+            getline(cin, searchBranch);
+            while(!in.eof()) {
+                getline(in, str1);
+                getline(in, str2);
+                getline(in, str3);
+                getline(in, str4);
+                if(searchBranch == str4) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
+                    count++;
+                    if(count < 2) { cout << "\n\n\t\t\tRequired Details : \n"; }
+                    cout << "\n"
+                            << "\t\t\tName          : " << setw(25) << str1 << endl
+                            << "\t\t\tRoll no       : " << setw(25) << str2 << endl
+                            << "\t\t\tCollege Email : " << setw(25) << str3 << endl
+                            << "\t\t\tBranch        : " << setw(25) << str4 << endl;
+                }
+            }
+            in.close();
+            if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
+        }
+        else if(choice == 0) {
+            return;
+        }
+        else {
+            cout << endl << "Incorrect Choice Entered ! " << endl; //break;
+        }       
+                
+
     }
 };
 class CollegeTeachers : public CollegeDetails {  //TEACHERS CLASS ***********************************
     void setDetails() {
         cin.ignore();
-        cout << "\t\t\tEnter ID : "; cin >> id;
-        cout << "\n\t\t\tEnter Name : "; cin >> name;
-        cout << "\t\t\tEnter Email : "; cin >> email;
-        cout << "\t\t\tEnter Department : "; cin >> department;
-        cout << "\t\t\tEnter Qualification : "; cin >> qualification;
+        cout << "\t\t\tEnter ID : "; getline(cin, id);
+        cout << "\n\t\t\tEnter Name : "; getline(cin, name);
+        cout << "\t\t\tEnter Email : "; getline(cin, email);
+        cout << "\t\t\tEnter Department : "; getline(cin, department);
+        cout << "\t\t\tEnter Qualification : "; getline(cin, qualification);
     }
     void createFile() { //FOR TEACHERS
         ofstream out;
@@ -87,26 +186,67 @@ class CollegeTeachers : public CollegeDetails {  //TEACHERS CLASS **************
     }
     void searchDetails() {  //FOR TEACHERS
         ifstream in;
-        string searchsName;
-        cout << "\n\t\t\tEnter Name of student to be searched : "; cin.ignore();  //FLUSHING STREAM...
-        getline(cin, searchsName);
-
+        string searchName, searchID;
+        string str1, str2, str3, str4, str5;
         in.open("Teachers.txt");
         int count = {};
-        string str1, str2, str3, str4, str5;
-        while(in >> str1 >> str2 >> str3 >> str4 >> str5) {
-            if(searchsName == str1) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
-                count++;
-                cout << "\n\n\t\t\tRequired Details : \n";
-                cout << "\n"
-                    << "\t\t\tName          : " << setw(25) << str1 << endl
-                    << "\t\t\tRoll no       : " << setw(25) << str2 << endl
-                    << "\t\t\tBranch        : " << setw(25) << str3 << endl
-                    << "\t\t\tCollege Email : " << setw(25) << str4 << endl;
+        cout << "\n\t\t\t1 <- Search teacher by 'Name'....." << endl
+             << "\t\t\t2 <- Search teacher by 'ID'....." << endl
+             << "\t\t\t0 <- To return to previous page....." << endl
+             << "\n\t\t ..... Enter your choice : "; cin >> choice;
+        if(choice == 1) {
+            cout << "\n\t\t\tEnter Name of teacher to be searched : "; cin.ignore();  //FLUSHING STREAM...
+            getline(cin, searchName);
+            while(!in.eof()) {          /* MAIN CORRECT CODE, commented to try something diff...below!! */
+                getline(in, str1);
+                getline(in, str2);
+                getline(in, str3);
+                getline(in, str4);
+                getline(in, str5);
+                if(searchName == str1) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
+                    count++;
+                    cout << "\n\n\t\t\tRequired Details : \n";
+                    cout << "\n"
+                                 << "\t\t\tName          : " << setw(25) << str1 << endl
+                                 << "\t\t\tID            : " << setw(25) << str2 << endl
+                                 << "\t\t\tEmail         : " << setw(25) << str3 << endl
+                                 << "\t\t\tDepartment    : " << setw(25) << str4 << endl
+                                 << "\t\t\tQualification : " << setw(25) << str5 << endl;
+                }
             }
+            in.close();
+            if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
         }
-        in.close();
-        if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
+        else if(choice == 2) {
+            cout << "\n\t\t\tEnter Roll Number of teacher to be searched : "; cin.ignore();  //FLUSHING STREAM...
+            getline(cin, searchID);
+            while(!in.eof()) {
+                getline(in, str1);
+                getline(in, str2);
+                getline(in, str3);
+                getline(in, str4);
+                getline(in, str5);
+                if(searchID == str2) {  //CHECKS IF ENTERED NAME MATCHES THE NAMES IN FILE...
+                    count++;
+                    cout << "\n\n\t\t\tRequired Details : \n";
+                    cout << "\n"
+                                 << "\t\t\tName          : " << setw(25) << str1 << endl
+                                 << "\t\t\tID            : " << setw(25) << str2 << endl
+                                 << "\t\t\tEmail         : " << setw(25) << str3 << endl
+                                 << "\t\t\tDepartment    : " << setw(25) << str4 << endl
+                                 << "\t\t\tQualification : " << setw(25) << str5 << endl;
+                }
+            }
+            in.close();
+            if(count == 0) { cout << "\n\t\t\tNo such Entries found !\n"; }
+        } else if(choice == 0) {
+            return;
+        }
+        else {
+            cout << endl << "Incorrect Choice Entered ! " << endl; //break;
+        }       
+                
+
     }
 };
 class DataBase : public CollegeStudents, public CollegeTeachers {  //DATABASE CLASS (MAINNNNNNN)....
@@ -135,10 +275,10 @@ class DataBase : public CollegeStudents, public CollegeTeachers {  //DATABASE CL
         }
         void printHeader() {
             cout << "\n\n\n\t\t-------------------------------------------------------------------------------------" << endl
-                << "\t\t<<<<<<<<<<<<<<<<<<<<<<<<<<<<| COLLEGE MANAGEMENT SYSTEM |>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl
-                << "\t\t-------------------------------------------------------------------------------------" << endl
-                << "\t\t\t\t\t\t\t\t\t    Creator : Haysten_D_Costa\n"
-                << "\t\t\t\t\t\t\t\t\t\t\t       21co56\n";
+                 << "\t\t<<<<<<<<<<<<<<<<<<<<<<<<<<<<| COLLEGE MANAGEMENT SYSTEM |>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl
+                 << "\t\t-------------------------------------------------------------------------------------" << endl
+                 << "\t\t\t\t\t\t\t\t\t    Creator : Haysten_D_Costa\n"
+                 << "\t\t\t\t\t\t\t\t\t\t\t       21co56\n";
         }
 };
 int DataBase :: visited = 0;
@@ -168,7 +308,7 @@ void DataBase :: externalPage() {  //DEFAULT PAGE
                     cout << "\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t        .....THANK YOU !.....\n\n\n\n\n";
                     exit(1);
                     break;
-            default:cout << endl << "Incorrect Choice Entered ! " << endl;
+            default:cout << endl << "Incorrect Choice Entered ! " << endl; 
         }
      } while(choice != 0);
 }
@@ -257,9 +397,9 @@ void DataBase::mainPage()  {  //MAIN REGISTRATION FUNCTION
                     CollegeStudents s;
                     CollegeDetails *p = &s;
                     cout << "\n\t\t\t1 -> Register New Student....." << endl
-                        << "\t\t\t2 -> Search For Student..... " << endl
-                        << "\t\t\t0 -> Exit Page....." << endl
-                        << "\n\t\t   .....Enter your choice : "; cin >> choice;
+                         << "\t\t\t2 -> Search For Student..... " << endl
+                         << "\t\t\t0 -> Exit Page....." << endl
+                         << "\n\t\t   .....Enter your choice : "; cin >> choice;
                     switch(choice) {
                         case 1 :cout << "\t\t\t";
                                 system("pause");
@@ -284,7 +424,7 @@ void DataBase::mainPage()  {  //MAIN REGISTRATION FUNCTION
                         case 0 :loginPage(); break;
                         default:cout << "\n\t\t\t    Incorrect Choice Entered !.....\t\t\t " << endl;
                                 cout << "\n\t\t\t";
-                                system("pause");
+                                system("pause"); 
                     }
                 }while(choice != 0);
 
@@ -328,7 +468,7 @@ void DataBase::mainPage()  {  //MAIN REGISTRATION FUNCTION
             case 0 :break;
             default:cout << "\n\t\t\t    Incorrect Choice Entered !.....\t\t\t " << endl;
                     cout << "\n\t\t\t";
-                    system("pause");
+                    system("pause"); 
         }
     }while(choice != 0);
 }
@@ -344,15 +484,8 @@ int main()
          << "\t\t\t\t\t\t\t\t\t    Creator : Haysten_D_Costa\n"
          << "\t\t\t\t\t\t\t\t\t\t\t       21co56\n"
          << "\n\t\t\t\t.................>>>>> WELCOME <<<<<.................\t\n";
-    cout << "\n\n\t\t\t\t\t  Setting up the system for you !\n";
-    // cout << "\n\t\t\t\t"; system("pause"); system("cls");
-    // cout << "\n\n\n\t\t-------------------------------------------------------------------------------------" << endl
-    //      << "\t\t<<<<<<<<<<<<<<<<<<<<<<<<<<<<| COLLEGE MANAGEMENT SYSTEM |>>>>>>>>>>>>>>>>>>>>>>>>>>>>" << endl
-    //      << "\t\t-------------------------------------------------------------------------------------" << endl
-    //      << "\t\t\t\t\t\t\t\t\t    Creator : Haysten_D_Costa\n"
-    //      << "\t\t\t\t\t\t\t\t\t\t\t       21co56\n"
-    //      << "\n\t\t\t\t.................>>>>> WELCOME <<<<<.................\t\n";
-
+    cout << "\n\t\t\t\t\t  Setting up the system for you !\n";
+    
     cout << "\n\n\n\t\t\tSet up a strong password : "; ch=getch();
     while(ch != 13) {
         p.push_back(ch);
